085695829aa5bd2e05faeee4c6d22004bf1825ec
Andrea Genovese <andregenovese2000@gmail.com>
2022-07-18 14:28:27 +0200

    inizio esercizio

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const Team = [
+                {
+                    'nome': 'Wayne Barnett',
+                    'ruolo': 'Founder & CEO',
+                    'foto': 'wayne-barnett-founder-ceo.jpg'
+                },
+
+                {
+                    'nome': 'Walter Gordon',
+                    'ruolo': 'Office Manager',
+                    'foto': 'walter-gordon-office-manager.jpg'
+                },
+
+                {
+                    'nome': 'Angela Lopez',
+                    'ruolo': 'Social Media Manage',
+                    'foto': 'angela-lopez-social-media-manager.jpg'
+                },
+
+                {
+                    'nome': 'Scott Estrada',
+                    'ruolo': 'Developer',
+                    'foto': 'scott-estrada-developer.jpg'
+                },
+
+                {
+                    'nome': 'Barbara Ramos',
+                    'ruolo': 'Graphic Designer',
+                    'foto': 'barbara-ramos-graphic-designer.jpg'
+                }
+    
+             ];
+
+             
